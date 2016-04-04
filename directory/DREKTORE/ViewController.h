efb80131400000ded4dcf1ae7a7119eaf7fbade5
Josh Yuan <joshua.yuano@gmail.com>
2016-04-04 12:15:40 -0700

    Adding the XCode project.

---
+++ directory/DREKTORE/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DREKTORE
+//
+//  Created by Joshua on 4/3/16.
+//  Copyright (c) 2016 Joshua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ directory/DREKTORE/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  DREKTORE
+//
+//  Created by Joshua on 4/3/16.
+//  Copyright (c) 2016 Joshua. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
