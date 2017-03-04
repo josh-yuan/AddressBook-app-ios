47648e380969016d60d7df3baac000b62c1b980a
Josh Yuan <joshua.yuano@gmail.com>
2017-03-03 20:34:44 -0800

    Started swift drektore project.

---
+++ swift_drektore/DREKTORE/DREKTORE/Source/UI/SearchViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SearchViewController.swift
+//  DREKTORE
+//
+//  Created by Joshua on 3/3/17.
+//  Copyright © 2017 Joshua. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
